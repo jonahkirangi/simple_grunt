fc7359478dba0316cdf07e6c5f8d49feb683afb9
Jonah Kirangi <jonahkirangi@gmail.com>
2014-02-12 09:49:18 -0800

    JSHint bugs cleared

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
     // define the files to lint
-    files: ['Gruntfile.js'],
+    all: ['Gruntfile.js', 'test/**/*.js'],
     // configure JSHint (documented at http://www.jshint.com/docs/)
     options: {
       // more options here if you want to override JSHint defaults
